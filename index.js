860f8340d14f47e1ff932ca69f5e91620eb4b528
Ahmet Hüseyin DOK <off.ahmethuseyindok@gmail.com>
2019-08-09 07:44:00 +0300

    added json body parser

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express')
 const app = express();
 const port =  process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 //import utils here if you wanna change protypes
 const WelcomeController = require('./controllers/welcome');
-
+//register middlewares here
+app.use(bodyParser.json());
 //routes can be registered here
 app.get('/', WelcomeController.index);
 
